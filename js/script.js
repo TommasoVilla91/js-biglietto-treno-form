6e4cc76a4a45e1283403b911876dc96c479318a0
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-09 15:39:11 +0100

    feat: exercise completed

--- js/script.js
+++ js/script.js
@@ -2,30 +2,36 @@
 const formElem = document.getElementById("user-form");
 const kmInput = document.getElementById("km");
 const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("username");
 
 // Elementi card
-const offer = document.getElementById("offer-card");
-const price = document.getElementById("price-card");
+const offerElem = document.getElementById("offer-card");
+const priceElem = document.getElementById("price-card");
+const nameElem = document.getElementById("name-card");
+const coachElem = document.getElementById("coach-card");
+const codeElem = document.getElementById("code-card");
+
 
 // Evento click
 formElem.addEventListener("submit", function(event) {
     event.preventDefault();
 
+    const username = nameInput.value.trim();
     const age = parseInt(ageInput.value.trim());
     const km = parseInt(kmInput.value.trim());
 
     // determinare sconto
     let discount20 = 20;
     let discount40 = 40;
-    let tariff = "Standard";
+    let tariff = "Tariffa Standard";
 
     let discount;
     if (age < 18) {
         discount = discount20;
-        tariff = "Minorenni";
+        tariff = "Tariffa Minorenni";
     } else if (age > 65) {
         discount = discount40;
-        tariff = "Over 65";
+        tariff = "Tariffa Over 65";
     } else {
         discount = 0;
     }
@@ -36,6 +42,21 @@ formElem.addEventListener("submit", function(event) {
     const finalPrice = (standardPrice - discountRate).toFixed(2);
 
     // inserimento valori card
-    offer.innerHTML = `${tariff}`;
-    price.innerHTML = `${finalPrice}€`
+    offerElem.innerHTML = `${tariff}`;
+    priceElem.innerHTML = `${finalPrice}€`;
+    nameElem.innerHTML = `${username}`;
+    coachElem.innerHTML = randomNum(1, 10);
+    codeElem.innerHTML = randomNum(10000, 99999);
+
 })
+
+// numero random
+/**
+ * generare numero random tra min e max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function randomNum(min, max) {
+    return Math.floor(Math.random() * max) + min; 
+}
